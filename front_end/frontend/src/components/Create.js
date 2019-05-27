5a3e677c8b28f01d5faeecc0dc9f347607f74081
ShriLingam23 <trnshri23@gmail.com>
2019-05-28 00:50:27 +0530

    Component files structuring

--- front_end/frontend/src/components/Create.js
+++ front_end/frontend/src/components/Create.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
+import axios from 'axios/index';
 import { Link } from 'react-router-dom';
 
 class Create extends Component {
